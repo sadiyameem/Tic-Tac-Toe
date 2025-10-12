31e7f6152758e36947b4e8f8e4bba03f2386e144
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-10-12 14:32:55 -0400

    added a go and score funtion

--- script.js
+++ script.js
@@ -2,16 +2,45 @@
 const gameboard = document.querySelector("#gameboard")
 const infoDisplay = document.querySelector("#info")
 const startCells = ["", "", "", "", "", "", "", "", ""]
+let go = "circle"
 
 infoDisplay.textContent = "Circle goes first"
 
 function createBoard() {
-  startCells.forEach((cell, index) => {
+  startCells.forEach((_cell, index) => {
     const cellElement = document.createElement("div")
     cellElement.classList.add("square")
+    cellElement.id = index
+    cellElement.addEventListener("click", addGo)
     gameboard.append(cellElement)
   })
 }
 createBoard()
+
+// addGo
+function addGo(e) {
+  const goDisplay = document.createElement("div")
+  goDisplay.classList.add(go)
+  e.target.append(goDisplay)
+  go = go === "circle" ? "cross" : "circle"
+  infoDisplay.textContent = "it is now " + go + "'s go"
+  e.target.removeEventListener("click", addGo)
+  checkScore()
+}
+
+// check score
+function checkScore() {
+  const allSquares = document.querySelectorAll(".square")
+  const winningCombos = [
+    [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
+  ]
+  winningCombos.forEach(array => {
+    const circleWins = array.every(cell => 
+      allSquares[cell].firstChild?.classList.contains("circle"))
+    if (circleWins) {
+      infoDisplay.textContent = "Circle Wins!"
+  }
+  })
+}
 
 
