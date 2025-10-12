c1bee47a1b0ad178b2bab6d5c6512b32239e06a5
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-10-12 13:52:07 -0400

    adding elements

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+// elements
+const gameboard = document.querySelector("#gameboard")
+const infoDisplay = document.querySelector("#info")
+const startCells = ["", "", "", "", "", "", "", "", ""]
 
+infoDisplay.textContent = "Circle goes first"
 
-
-
-
-
-
+function createBoard() {
+  startCells.forEach((cell, index) => {
+    const cellElement = document.createElement("div")
+    cellElement.classList.add("square")
+    gameboard.append(cellElement)
+  })
+}
+createBoard()
 
 
